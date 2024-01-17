42a3951a0fdce9713e5fff127efc56058129bb72
Yabsira Tekalegn <yabsiratekalegn53@gmail.com>
2024-01-17 18:22:31 +0000

    Jan 17, 2024, 10:22 AM

--- 0x01-python-if_else_loops_functions/7-main.py
+++ 0x01-python-if_else_loops_functions/7-main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-islower = __import__('7-islower').islower
 
+islower = __import__('7-islower').islower
+#the code imports a function named islower from a module named 7-islower
+#this function is designed to determine whether a given character is lowercase
 print("a is {}".format("lower" if islower("a") else "upper"))
 print("H is {}".format("lower" if islower("H") else "upper"))
 print("A is {}".format("lower" if islower("A") else "upper"))
