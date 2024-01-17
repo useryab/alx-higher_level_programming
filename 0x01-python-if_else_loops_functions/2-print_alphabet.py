cd3ab7e766989d97127b1f4a4e7b0fba57926724
Yabsira Tekalegn <yabsiratekalegn53@gmail.com>
2024-01-17 16:15:36 +0000

    Jan 17, 2024, 8:15 AM

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -2,4 +2,4 @@
 start_codepoint = ord('a')
 end_codepoint = ord('z')
 for codepoint in range(start_codepoint, end_codepoint + 1):
-   print(chr(codepoint), end="")
+    print(chr(codepoint), end="")
