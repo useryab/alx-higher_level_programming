3c6ccea219984050f05bbae61185c56bbdd6b9f7
Yabsira Tekalegn <yabsiratekalegn53@gmail.com>
2024-01-19 20:40:31 +0000

    Jan 19, 2024, 12:40 PM

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
 from sys import argv
+argc = len(argv)
+if argc < 2:
+    print(f"{argc -1} arguments:")
+elif argc == 2:
+    print(f"{argc - 1} argument:")
+else:
+    print(f"{argc - 1} arguments:")
+for n in range(1, argc):
+    print(f"{n}: {argv[n]} ")    
