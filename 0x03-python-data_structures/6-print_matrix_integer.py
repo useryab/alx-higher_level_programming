7811515fa274187ef122a866872d0f9e720a3e5a
Yabsira Tekalegn <yabsiratekalegn53@gmail.com>
2024-01-23 08:23:34 +0000

    Jan 23, 2024, 12:23 AM

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if
+    if len(matrix) == 1 and any(matrix) is False:
+        print()
+    for row in matrix:
+        count = 0
+        for n in row:
+            if count == len(row) - 1:
+                print(f"{n}", end="")
+                print()
+            else:
+                print(f"{n}", end=" ")
+            count += 1
