4b187c8c61181705897d750330631b16deb38ac2
Yabsira Tekalegn <yabsiratekalegn53@gmail.com>
2024-01-23 08:10:47 +0000

    Jan 23, 2024, 12:10 AM

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if 
+    if idx < 0 or idx >= len(my_list):
         return
-    else
-        return()
+    else:
+        return(my_list[idx])
