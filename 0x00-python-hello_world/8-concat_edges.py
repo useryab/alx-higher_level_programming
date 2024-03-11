3de26db528dafe5d97db819ce95fec642b5bc095
Yabsira Tekalegn <yabsiratekalegn53@gmail.com>
2024-03-11 07:03:55 +0000

    Mar 10, 2024, 11:03 PM

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str.find("object-oriented programming with Python"))
+str = str[39:67] + str[107:112] + str[:6]
+print(str)
