4c41dbf6040b7e3993fdd1a29c55ba5a34fb93f5
Yabsira Tekalegn <yabsiratekalegn53@gmail.com>
2024-03-13 18:56:39 -0800

    merge conflict

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -2,27 +2,30 @@
 #include "lists.h"
 
 /**
- * check_cycle: checks if a list contains a cycle
- * @list: singly-linked list
- * Return: if there is a cycle  1
- *         if not then 0
-*/
-int check_cycle(listint_t *list);
+ * check_cycle - Checks if a singly-linked list contains a cycle.
+ * @list: A singly-linked list.
+ *
+ * Return: If there is no cycle - 0.
+ *         If there is a cycle - 1.
+ */
+int check_cycle(listint_t *list)
 {
-    listint_s *turtle, *hare;
+	listint_t *turtle, *hare;
 
-    if(list == NULL || list->next == NULL)
-            return (0);
-    turtle = list->next;
-    hare = list->next;
+	if (list == NULL || list->next == NULL)
+		return (0);
 
-    while (turtle && hare && hare->next)
-    {
-        if (turtle == hare)
-                return (1);
-        turtle = turtle->next;
-        hare = hare->next->next
-    }
+	turtle = list->next;
+	hare = list->next->next;
 
-        return (0)
+	while (turtle && hare && hare->next)
+	{
+		if (turtle == hare)
+			return (1);
+
+		turtle = turtle->next;
+		hare = hare->next->next;
+	}
+
+	return (0);
 }
