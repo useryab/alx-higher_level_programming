77feaa409b1b8e0f2a8d552dac02fdcfb354b788
Yabsira Tekalegn <yabsiratekalegn53@gmail.com>
2024-03-14 03:25:48 +0000

    Mar 13, 2024, 7:25 PM

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ for i in range(0, 10):
     for j in range(0, 10):
         if i < j:
             if i < 8:
-                print("{}{}, ".format(i, j), end="")
+                print(f"{i}{j}, ", end="")
             else:
-                print("{}{}".format(i, j))
+                print(f"{i}{j}")
