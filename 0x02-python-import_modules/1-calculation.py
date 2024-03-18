a39d39724002bdeca78b2efc053688c1e9f25e18
Yabsira Tekalegn <yabsiratekalegn53@gmail.com>
2024-03-18 04:59:20 +0000

    Mar 17, 2024, 8:59 PM

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    print(f"{a} + {b} = {a+b}")
+    print(f"{a} - {b} = {a-b}")
+    print(f"{a} * {b} = {a*b}")
+    print(f"{a} / {b} = {a/b}")
