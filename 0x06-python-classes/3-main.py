0f38a75c1328fea3f24d7979ff26d3eebc9d95c7
Yabsira Tekalegn <yabsiratekalegn53@gmail.com>
2024-04-08 04:24:29 +0000

    Apr 7, 2024, 8:24 PM

--- 0x06-python-classes/3-main.py
+++ 0x06-python-classes/3-main.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-Square = __import__('102-square').Square
+Square = __import__('3-square').Square
 
-s_5 = Square(5)
-s_6 = Square(6)
+my_square_1 = Square(3)
+print("Area: {}".format(my_square_1.area()))
 
-if s_5 < s_6:
-    print("Square 5 < Square 6")
-if s_5 <= s_6:
-    print("Square 5 <= Square 6")
-if s_5 == s_6:
-    print("Square 5 == Square 6")
-if s_5 != s_6:
-    print("Square 5 != Square 6")
-if s_5 > s_6:
-    print("Square 5 > Square 6")
-if s_5 >= s_6:
-    print("Square 5 >= Square 6")
+try:
+    print(my_square_1.size)
+except Exception as e:
+    print(e)
+
+try:
+    print(my_square_1.__size)
+except Exception as e:
+    print(e)
+
+my_square_2 = Square(5)
+print("Area: {}".format(my_square_2.area()))
