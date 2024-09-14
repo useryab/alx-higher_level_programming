c565d45e2f7f41de0111b48b921f0d9f702db214
Yabsira Tekalegn <yabsiratekalegn53@gmail.com>
2024-09-14 07:28:16 -0800

    check

--- 0x04-python-more_data_structures/11-main.py
+++ 0x04-python-more_data_structures/11-main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/python3
 multiply_list_map = __import__('11-multiply_list_map').multiply_list_map
 
@@ -6,12 +5,3 @@ my_list = [1, 2, 3, 4, 6]
 new_list = multiply_list_map(my_list, 4)
 print(new_list)
 print(my_list)
-=======
-#!/usr/bin/python3
-multiply_list_map = __import__('11-multiply_list_map').multiply_list_map
-
-my_list = [1, 2, 3, 4, 6]
-new_list = multiply_list_map(my_list, 4)
-print(new_list)
-print(my_list)
->>>>>>> 18089313bf331b15816d82d256d82ac0c96c6a2a
